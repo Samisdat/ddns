34a81306bcbb65d376a228af8f147d55f08830c8
samisdat <bastian.pertz@gmail.com>
2015-05-07 23:49:25 +0200

    options

--- control/control.sh
+++ control/control.sh
@@ -3,31 +3,50 @@
 # Usage info
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-hv] [-f OUTFILE] [FILE]...
+Usage: ${0##*/} [-h] [-c ns.example.de example.de] [FILE]...
 Do stuff with FILE and write the result to standard output. With no FILE
 or when FILE is -, read standard input.
 
-    -h          display this help and exit
-    -f OUTFILE  write the result to OUTFILE instead of standard output.
+    -h, -help          display this help and exit
+    -c, -config          create configfile
+    -cf OUTFILE  write the result to OUTFILE instead of standard output.
     -v          verbose mode. Can be used multiple times for increased
                 verbosity.
 EOF
 }
+
+create_config(){
+	local NAMESERVER=$1
+	local DYNAMIC_DOMAIN=$3
+
+	echo $NAMESERVER
+	echo $DYNAMIC_DOMAIN
+
+}
+
 
 # Initialize our own variables:
 output_file=""
 verbose=0
 
 OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
-while getopts "hvf:" opt; do
+while getopts "helpexample-config:config" opt; do
     case "$opt" in
         h)
             show_help
             exit 0
             ;;
-        v)  verbose=$((verbose+1))
+        help)
+            show_help
+            exit 0
             ;;
-        f)  output_file=$OPTARG
+        c)
+            create_config
+            exit 0
+            ;;
+        config)
+            create_config
+            exit 0
             ;;
         '?')
             show_help >&2
