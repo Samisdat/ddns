464059b6149bdf168eeba361d4559cdf977a16d5
samisdat <bastian.pertz@gmail.com>
2016-03-03 22:55:03 +0000

    coverage with less clobber

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,19 +19,7 @@ module.exports = function(grunt) {
             coverage: {
                 src: 'test/**/*.js' // a folder works nicely
             }
-        },
-        'istanbul_check_coverage': {
-            default: {
-                options: {
-                    coverageFolder: 'coverage/', // will check both coverage folders and merge the coverage results
-                    check: {
-                        lines: 80,
-                        statements: 80
-                    }
-                }
-            }
         }
-
     });
 
 
