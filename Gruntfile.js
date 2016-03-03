f06e99076dc7fa0be4c4781ac9830c53bec316c3
samisdat <bastian.pertz@gmail.com>
2016-03-03 22:16:02 +0000

    use eslint and smaller Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,14 @@
 module.exports = function(grunt) {
     "use strict";
 
-    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+    
+    require('load-grunt-tasks')(grunt, {pattern: [
+        'grunt-*', 
+        '@*/grunt-*',
+        'gruntify-eslint*'
+    ]});
 
-    var path = require('path');
-    require('load-grunt-config')(grunt,{
-        configPath:path.join(process.cwd(), 'grunt')
-    });
+    require('load-grunt-config')(grunt);
 
-    grunt.registerTask('test', 'Build autoload file and run tests.', function(testFilter) {
-        grunt.task.run('autoload');
-        if (!grunt.option('lite')) {
-            grunt.task.run('clean:coverage');
-        }
-        if (testFilter === undefined) {
-            grunt.task.run('exec:phpunit');
-        }
-        else {
-            grunt.task.run('exec:phpunit:' + testFilter);
-        }
-    });
 };
---
+++ grunt/eslint.js
@@ -0,0 +1,9 @@
+/*global module */
+
+var eslint = {
+	src: ['grunt/lib/qfs.js']
+};
+
+
+
+module.exports = eslint;
