78aca506120e4f9b34c98c64369c53bff15d9c18
samisdat <bastian.pertz@gmail.com>
2016-03-07 21:53:06 +0000

    proof of concept: testing with virtual filesystem

--- test/qfs.js
+++ test/qfs.js
@@ -1,3 +1,11 @@
+var unionfs = require('unionfs');
+var memfs = require('memfs');
+var fs = require('fs');
+var mem = new memfs.Volume;
+mem.mountSync('/test/dir/dir2', {
+    "test.js": "console.log(123);",
+    "dir/hello.js": "console.log('hello world');",
+    "dir/more/hello.js": "console.log('hello world');",
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 
